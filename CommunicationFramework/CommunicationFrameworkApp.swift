37073763a4bad46a24a6f2cc7c64ca2f4dd0f660
conrad030 <conrad.felgentreff@web.de>
2022-04-21 17:27:40 +0200

    Initial Commit

---
+++ CommunicationFramework/CommunicationFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunicationFrameworkApp.swift
+//  CommunicationFramework
+//
+//  Created by Conrad Felgentreff on 21.04.22.
+//
+
+import SwiftUI
+
+@main
+struct CommunicationFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
